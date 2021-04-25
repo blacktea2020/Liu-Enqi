e12fdde7c59cb339685db546a3baf49ce94a43cb
blacktea2020 <73392691+blacktea2020@users.noreply.github.com>
2021-04-25 18:23:43 +0800

    homework

---
+++ Chapter 2/Homework03.java
@@ -0,0 +1,13 @@
+public class Homework03{
+    public static void main(String[] args) {
+        String book1 = "JAVA核心技术";
+        String book2 = "三国演义";
+        String gender1 = "男";
+        String gender2 = "女";
+        double price1 = 66.66;
+        double price2 = 11.11;
+        System.out.println(book1 + book2);
+        System.out.println(gender1 + gender2);
+        System.out.println(price1 + price2);
+    }
+}
---
+++ Chapter 2/Homework04.java
@@ -0,0 +1,10 @@
+public class Homework04{
+    public static void main(String[] args) {
+        String name = "xx";
+        int age = 24;
+        double grade = 100.00;
+        String gender = "男";
+        String hoppy = "java";
+        System.out.println("姓名\t\t年龄\t成绩\t性别\t爱好"+"\n"+name+"\t\t"+age+"\t"+grade+"\t"+gender+"\t"+hoppy);
+    }
+}
